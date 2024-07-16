fc4dfc02d5e3ac8f140f294207e0a32c51f8bdbb
Pravin Patil <pravinshivajipatil37@gmail.com>
2024-07-16 10:08:44 +0000

    Golf

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,49 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             renderBall: false,
-            posi : 0,
+            posi: 0,
             ballPosition: { left: "0px" }
         };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
-
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
+        this.renderBallOrButton = this.renderBallOrButton.bind(this);
+        this.buttonClickHandler = this.buttonClickHandler.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
-    // bind ArrowRight keydown event
+    buttonClickHandler() {
+        this.setState({ renderBall: true });
+    }
+
+    renderBallOrButton() {
+        if (this.state.renderBall) {
+            return <div className="ball" style={this.state.ballPosition}></div>;
+        } else {
+            return <button className="start" onClick={this.buttonClickHandler}>Start</button>;
+        }
+    }
+
+    handleKeyDown(event) {
+        if (event.key === 'ArrowRight' || event.keyCode === 39) {
+            this.setState((prevState) => {
+                const newPosi = prevState.posi + 5;
+                return {
+                    posi: newPosi,
+                    ballPosition: { left: `${newPosi}px` }
+                };
+            });
+        }
+    }
+
     componentDidMount() {
-      
+        document.addEventListener('keydown', this.handleKeyDown);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyDown);
     }
 
     render() {
@@ -34,9 +51,8 @@ class App extends Component {
             <div className="playground">
                 {this.renderBallOrButton()}
             </div>
-        )
+        );
     }
 }
-
 
 export default App;
